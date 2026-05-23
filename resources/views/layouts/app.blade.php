0877003008491d800d64df6d84d6f6bb4be23f5e
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-05-23 15:57:29 +0530

    Add SmartAgro branded favicon (green leaf logo)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartAgro | @yield('title', 'Insect Pesticide Identification in Crops')</title>
     <meta name="description" content="@yield('meta_description', 'SmartAgro - Intelligent crop monitoring and chemical residue analysis platform driving sustainable farming across India.')">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet">
